53606b3b1438681d4b145849f82f86118a4671be
Kristopher Chayadi <kristopher.chayadi1688@gmail.com>
2018-08-15 18:59:08 +0700

    Adding Page View [Thanks Ricky]

--- ARganize/Controller/InfoPageViewController.swift
+++ ARganize/Controller/InfoPageViewController.swift
@@ -8,26 +8,71 @@
 
 import UIKit
 
-class InfoPageViewController: UIPageViewController {
+class InfoPageViewController: UIPageViewController,UIPageViewControllerDataSource,UIPageViewControllerDelegate {
 
-    
+    let pageContentViewControllers = [
+        UIViewController(nibName: "InfoPage1", bundle: nil),
+        UIViewController(nibName: "InfoPage2", bundle: nil),
+        UIViewController(nibName: "InfoPage3", bundle: nil)
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        self.dataSource = dataSource
-        // Do any additional setup after loading the view.
+        self.dataSource = self
+        self.setViewControllers([pageContentViewControllers.first!], direction: UIPageViewController.NavigationDirection.forward, animated: true, completion: nil)
+        
+//        InfoPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        
+//        pageViewController = new UIPageViewController(UIPageViewControllerTransitionStyle.Scr‌​oll, UIPageViewControllerNavigationOrientation.Horizontal)
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return pageContentViewControllers.count
     }
-    */
-
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0;
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = pageContentViewControllers.index(of: viewController) else {
+            return nil
+        }
+        let nextIndex = index + 1
+        guard pageContentViewControllers.count != nextIndex else {
+            //return orderedViewController.first
+            return nil
+        }
+        
+        guard pageContentViewControllers.count > nextIndex else {
+            return nil
+        }
+        
+        return pageContentViewControllers[nextIndex]
+        //let index = pageContentViewControllers.lastIndex(of: pageContentViewControllers)//find(pageContentViewControllers, viewController)
+//        if index == nil || index + 1 == pageContentViewControllers.count {
+//            return nil
+//        } else {
+//            return pageContentViewControllers[index + 1]
+//        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = pageContentViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = index - 1
+        
+        guard previousIndex >= 0 else {
+            //return orderedViewController.last
+            return nil
+        }
+        
+        guard pageContentViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return pageContentViewControllers[previousIndex]
+    }
 }
