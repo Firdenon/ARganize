1c2f023cac597df911f3cc8c9c9cafe44fcc3fd1
Julius Michael <juliusmichael13@gmail.com>
2018-08-20 16:00:32 +0700

    SplashScreen + InfoPage

--- ARganize/Controller/InfoPage1.swift
+++ ARganize/Controller/InfoPage1.swift
@@ -13,7 +13,7 @@ class InfoPage1: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
 
